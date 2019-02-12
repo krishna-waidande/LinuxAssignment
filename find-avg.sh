b929d7d48ff3f888bd4cd375ea6d7af0e0431bee
krishna-waidande <krishnawaidande105@gmail.com>
2019-02-12 17:59:32 +0530

    Added validation to input & renamed variable output -> sum

--- find-avg.sh
+++ find-avg.sh
@@ -4,16 +4,22 @@ echo "Program which calculate the average of the specified numbers";
 echo "How many numbers you want to enter?";
 read count;
 
+if [ $count -le 0 ]
+then
+  echo "Invalid input"
+  exit;
+fi
+
 counter=0;
-output=0;
+sum=0;
 
 echo "Enter the numbers";
 
 while [ $counter -lt $count ]
 do
   read num;
-  output=$((output+num))
+  sum=$((output+num))
   counter=$((counter+1))
 done
 
-echo "Average of numbers is : $((output/count))";
+echo "Average of numbers is : $((sum/count))";
